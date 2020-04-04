ddd315fdc00ad8f08834d9e7cc176925908487f2
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-04 16:08:28 -0600

    FEATURE: Add main file to run the plugin

---
+++ wp-gif-player-play-and-pause.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: WP GIF Player - Play & Pause
+ * Plugin URI:  https://roelmagdaleno.com
+ * Description: Attach a GIF player into your pages and posts.
+ * Version:     0.1.0
+ * Author:      Roel Magdaleno
+ * Author URI:  https://roelmagdaleno.com
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
