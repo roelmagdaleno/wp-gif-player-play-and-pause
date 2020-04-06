4df4ebb16e75e152e4c5836adc048c4f697ac707
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-05 19:06:17 -0500

    IMPROVE: New files added in the required-files.php file

--- includes/required-files.php
+++ includes/required-files.php
@@ -5,10 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once 'constants.php';
+require_once 'helpers.php';
 
 // HTML Classes
 require_once 'html/class-html-radio.php';
 
+// Traits
+require_once 'traits/trait-thumbnail-creator.php';
+
 require_once 'class-wp-gp-pp-options.php';
+require_once 'class-wp-gp-pp-media-uploader.php';
 require_once 'class-wp-gp-pp-shortcode.php';
 require_once 'class-wp-gp-pp.php';
