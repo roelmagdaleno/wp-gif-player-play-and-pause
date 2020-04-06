76e4fdb8fb3f7ca1958e41df3dd07a031346661f
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-06 00:51:05 -0500

    FEATURE: Video Creator trait added into the required-files.php file

--- includes/required-files.php
+++ includes/required-files.php
@@ -4,6 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// General Classes
 require_once 'constants.php';
 require_once 'helpers.php';
 
@@ -12,7 +13,9 @@ require_once 'html/class-html-radio.php';
 
 // Traits
 require_once 'traits/trait-thumbnail-creator.php';
+require_once 'traits/trait-video-creator.php';
 
+// Main Classes
 require_once 'class-wp-gp-pp-options.php';
 require_once 'class-wp-gp-pp-media-uploader.php';
 require_once 'class-wp-gp-pp-shortcode.php';
