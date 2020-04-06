3caa011e26177fdb2e69d60bd655a575eb729d87
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-05 19:43:43 -0500

    FEATURE: Load the respective JS file according to the GIF method

--- includes/class-wp-gp-pp-shortcode.php
+++ includes/class-wp-gp-pp-shortcode.php
@@ -12,11 +12,22 @@ if ( ! class_exists( 'WP_GP_PP_Shortcode' ) ) {
 	 */
 	class WP_GP_PP_Shortcode {
 		/**
+		 * The plugin settings.
+		 *
+		 * @since  0.1.0
+		 * @access private
+		 *
+		 * @var    array   $settings   The plugin settings.
+		 */
+		private $settings;
+
+		/**
 		 * Initialize the shortcode to render the GIF player.
 		 *
 		 * @since 0.1.0
 		 */
 		public function __construct() {
+			$this->settings = get_option( 'wp_gp_pp_settings' );
 			add_shortcode( 'gif-player', array( $this, 'render_gif_player' ) );
 		}
 
@@ -32,7 +43,7 @@ if ( ! class_exists( 'WP_GP_PP_Shortcode' ) ) {
 		 * @return string              The GIF player.
 		 */
 		public function render_gif_player( $raw_atts ) {
-			if ( ! isset( $raw_atts['src'] ) || empty( $raw_atts['src'] ) ) {
+			if ( ! isset( $raw_atts['id'] ) || empty( $raw_atts['id'] ) ) {
 				return '';
 			}
 
@@ -45,40 +56,59 @@ if ( ! class_exists( 'WP_GP_PP_Shortcode' ) ) {
 				WP_GP_PP_VERSION
 			);
 
+			if ( 'canvas' === $this->settings['gif_method'] ) {
+				wp_enqueue_script(
+					'wp-gp-pp-libgif.js',
+					plugins_url( 'assets/js/libgif.js', __DIR__ ),
+					null,
+					WP_GP_PP_VERSION,
+					$in_footer
+				);
+			}
+
 			wp_enqueue_script(
-				'wp-gp-pp-libgif.js',
-				plugins_url( 'assets/js/libgif.js', __DIR__ ),
+				'wp-gp-pp-' . $this->settings['gif_method'] . '.js',
+				plugins_url( 'assets/js/wp-gp-pp-' . $this->settings['gif_method'] . '.js', __DIR__ ),
 				null,
 				WP_GP_PP_VERSION,
 				$in_footer
 			);
 
-			wp_enqueue_script(
-				'wp-gp-pp.js',
-				plugins_url( 'assets/js/wp-gp-pp.js', __DIR__ ),
-				null,
-				WP_GP_PP_VERSION,
-				$in_footer
-			);
+			$attachment_id = (int) $raw_atts['id'];
 
-			$atts = shortcode_atts( array(
-				'id'        => 'wp-gp-pp--id-' . wp_rand(),
-				'auto_play' => '0',
-				'src'       => $raw_atts['src'] ? trim( $raw_atts['src'] ) : '',
-			), $raw_atts );
-
-			if ( empty( $atts['src'] ) ) {
+			if ( ! wp_gp_pp_is_gif( $attachment_id ) ) {
 				return '';
 			}
 
-			$maybe_playing = ( '1' === $atts['auto_play'] ) ? 'is-playing' : '';
+			$size       = apply_filters( 'wp_gp_pp_attachment_size', 'full' );
+			$gif_source = wp_get_attachment_image_src( $attachment_id, $size );
 
-			$image  = '<div class="wp-gp-pp-container">';
-			$image .= '<img src="' . esc_attr( $atts['src'] ) . '" id="' . esc_attr( $atts['id'] ) . '" ';
-			$image .= 'rel:animated_src="' . esc_attr( $atts['src'] ) . '" ';
-			$image .= 'rel:auto_play="' . esc_attr( $atts['auto_play'] ) . '" class="wp-gp-pp-gif-player">';
+			if ( ! $gif_source ) {
+				return '';
+			}
+
+			$raw_atts['id'] = 'wp-gp-pp--id-' . $attachment_id;
+
+			$atts = shortcode_atts( array(
+				'id'     => $raw_atts['id'],
+				'width'  => $gif_source['1'],
+				'height' => $gif_source['2'],
+			), $raw_atts );
+
+			$thumbnail = str_replace( '.gif', '_gif_thumbnail.jpeg', $gif_source[0] );
+			$width     = esc_attr( $atts['width'] );
+			$height    = esc_attr( $atts['height'] );
+
+			$image  = '<div class="wp-gp-pp-container" style="width: ' . $width . 'px; height: ' . $height . 'px">';
+
+			$image .= '<img src="' . esc_attr( $thumbnail ) . '" id="' . esc_attr( $atts['id'] ) . '--thumbnail" ';
+			$image .= 'class="wp-gp-pp-gif-thumbnail" width="' . $width . '" height="' . $height . '" alt="">';
+
+			$image .= '<img src="" id="' . esc_attr( $atts['id'] ) . '" ';
+			$image .= 'class="wp-gp-pp-gif" width="' . $width . '" height="' . $height . '" alt="">';
+
 			$image .= '<div class="wp-gp-pp-overlay"> ';
-			$image .= '<div class="wp-gp-pp-play-button ' . $maybe_playing . '">GIF</div> ';
+			$image .= '<div class="wp-gp-pp-play-button">GIF</div> ';
 			$image .= '</div> </div>';
 
 			return $image;
