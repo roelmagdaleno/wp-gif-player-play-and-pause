466e828d53c48a17219e1a328b06dec0f0ed8c93
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-08 21:27:42 -0500

    FEATURE: Gutenberg namespace added as constant

--- includes/constants.php
+++ includes/constants.php
@@ -4,4 +4,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * The plugin version.
+ *
+ * @since 0.1.0
+ */
 define( 'WP_GP_PP_VERSION', '0.1.0' );
+
+/**
+ * The Gutenberg block namespace.
+ *
+ * @since 0.1.0
+ */
+define( 'WP_GP_PP_GUTENBERG_NAMESPACE', 'roelmagdaleno/gif-player' );
